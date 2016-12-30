df730f34410b9c794dce155473c79a309edcd082
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-12-30 17:47:00 -0500

    adding clean up steps

--- pagerank-process.sh
+++ pagerank-process.sh
@@ -7,3 +7,9 @@ sed -e "s/\${COLLECTION}/$1/g" pagerank-template.scala > $1.scala
 
 # execute in Spark Shell
 /home/ubuntu/project/spark-1.6.1-bin-hadoop2.6/bin/spark-shell -i /home/ubuntu/WALK-PageRank/$1.scala --driver-memory 45G --jars /home/ubuntu/project/warcbase/warcbase-core/target/warcbase-core-0.1.0-SNAPSHOT-fatjar.jar | tee /home/ubuntu/WALK-PageRank/$1-pagerank.log
+
+# clean up
+cat /data/derivatives/pagerank/$1-links/part* > $1-links.txt
+mv $1-links.txt /data/derivatives/pagerank
+cat /data/derivatives/pagerank/$1-nodes/part* > $1-nodes.txt
+mv $1-nodes.txt /data/derivatives/pagerank
