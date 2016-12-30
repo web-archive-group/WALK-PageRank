f615d8f9d459beeaca5f4b54cda20c18e96d5011
Ian Milligan <ianmilligan1@gmail.com>
2016-12-30 22:26:49 +0000

    modified to static, 15 iterations

--- pagerank-template.scala
+++ pagerank-template.scala
@@ -4,6 +4,6 @@ import org.warcbase.spark.matchbox.RecordLoader
 import org.warcbase.spark.matchbox.ExtractGraph
 
 val recs=RecordLoader.loadArchives("/data/${COLLECTION}/*.gz", sc)
-val graph = ExtractGraph(recs, dynamic=true)
+val graph = ExtractGraph(recs, dynamic=false, numIter = 15)
 graph.writeAsJson("/data/derivatives/pagerank/${COLLECTION}-nodes", "/data/derivatives/pagerank/${COLLECTION}-links")
 exit
