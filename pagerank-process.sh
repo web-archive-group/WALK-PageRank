85f09fda77e80dfc46e6f4908bd51bae374f8028
Ian Milligan <ianmilligan1@gmail.com>
2016-12-30 22:32:19 +0000

    fixing path errors

--- pagerank-process.sh
+++ pagerank-process.sh
@@ -6,4 +6,4 @@
 sed -e "s/\${COLLECTION}/$1/g" pagerank-template.scala > $1.scala
 
 # execute in Spark Shell
-/home/ubuntu/project/spark-1.6.1-bin-hadoop2.6/bin/spark-shell -i /home/ubuntu/production/$1.scala --driver-memory 45G --jars /home/ubuntu/project/warcbase/warcbase-core/target/warcbase-core-0.1.0-SNAPSHOT-fatjar.jar | tee /home/ubuntu/production/$1-pagerank.log
+/home/ubuntu/project/spark-1.6.1-bin-hadoop2.6/bin/spark-shell -i /home/ubuntu/WALK-PageRank/$1.scala --driver-memory 45G --jars /home/ubuntu/project/warcbase/warcbase-core/target/warcbase-core-0.1.0-SNAPSHOT-fatjar.jar | tee /home/ubuntu/WALK-PageRank/$1-pagerank.log
