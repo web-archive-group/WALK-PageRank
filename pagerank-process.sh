6cb16ff71ed63668888e8c0391c9e38d4d980173
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-12-30 18:54:25 -0500

    and looking good

--- pagerank-process.sh
+++ pagerank-process.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# USAGE: sh warcbase-process.sh COLLECTIONNAME
-# i.e. sh warcbase-process.sh WAHR_ymmfire
 
 # create the new scala script to run
 sed -e "s/\${COLLECTION}/$1/g" pagerank-template.scala > $1.scala
@@ -9,4 +7,4 @@ sed -e "s/\${COLLECTION}/$1/g" pagerank-template.scala > $1.scala
 /home/ubuntu/project/spark-1.6.1-bin-hadoop2.6/bin/spark-shell -i /home/ubuntu/WALK-PageRank/$1.scala --driver-memory 45G --jars /home/ubuntu/project/warcbase/warcbase-core/target/warcbase-core-0.1.0-SNAPSHOT-fatjar.jar | tee /home/ubuntu/WALK-PageRank/$1-pagerank.log
 
 # clean up
-jq -c -n --slurpfile nodes <(cat /data/derivatives/pagerank/$1-nodes/part-*) --slurpfile links <(cat /data/derivatives/pagerank/$1-links/part-*) '{nodes: $nodes, links: $links}' > $1-graph.json
+jq -c -n --slurpfile nodes <(cat /data/derivatives/pagerank/$1-nodes/part-*) --slurpfile links <(cat /data/derivatives/pagerank/$1-links/part-*) '{nodes: $nodes, links: $links}' > /data/derivatives/pagerank/$1-graph.json
