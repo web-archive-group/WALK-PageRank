ffd957927bb6f28d251c56a5fd5705e55f058eb8
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-12-30 17:27:14 -0500

    first check in

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WALK-PageRank
-Calculating PageRank on WALK Collections
+
+This repository modifies WALK templates in order to run PageRank on collections. Dynamic PageRank is probably too intensive, so for at least a beginning point we will run 15 iterations per collection.
---
+++ pagerank-process.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# USAGE: sh warcbase-process.sh COLLECTIONNAME
+# i.e. sh warcbase-process.sh WAHR_ymmfire
+
+# create the new scala script to run
+sed -e "s/\${COLLECTION}/$1/g" pagerank-template.scala > $1.scala
+
+# execute in Spark Shell
+/home/ubuntu/project/spark-1.6.1-bin-hadoop2.6/bin/spark-shell -i /home/ubuntu/production/$1.scala --driver-memory 45G --jars /home/ubuntu/project/warcbase/warcbase-core/target/warcbase-core-0.1.0-SNAPSHOT-fatjar.jar | tee /home/ubuntu/production/$1-pagerank.log
